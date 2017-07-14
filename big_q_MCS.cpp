cb866d812f75adbf7490c0c2dc68cc8e65aaa721
svalenti52 <svalenti52@yahoo.com>
2017-07-14 07:33:43 -0700

    Added documentation to big_q_MCS.cpp.

--- big_q_MCS.cpp
+++ big_q_MCS.cpp
@@ -5,7 +5,7 @@
  * \brief From Random Unit Interval set, choose numerator N to be max, denominator D to be min
  * so N/D is largest from set.
  *
- * \details Probability that N/D is larger than parametrized number.
+ * \details Probability that N/D is larger than parametrized number (threshold).
  */
 
 #include <val/montecarlo/Chronology.h>
@@ -17,21 +17,29 @@ using DIST = DistributionType;
 int main(int argc, char** argv) {
 
 
-    const double k = atof(argv[1]);
-    const int N = atoi(argv[2]);
+    const double k = atof(argv[1]); ///> Threshold value (k is variable name taken from text)
+    const int N = atoi(argv[2]);    ///> Number of values in unit interval to generate
 
     Distribution<double, DIST::UniformReal> distribution(0.0, 1.0, N);
 
     auto condition_met = [k](Distribution<double, DIST::UniformReal>& random_Qvals,
-            double& iv) -> bool { ///> condition met?
+            double& interim_count) -> bool { ///> condition met?
 
-        ///> iv defaults to 1.0
+        ///> interim_count defaults to 1.0, correct for this problem
 
+        /**
+         * minmax_element creates a pair composed of the minimum (first) and maximum
+         * (second) values.
+         */
         auto pr = std::minmax_element(random_Qvals.events.begin(), random_Qvals.events.end());
 
-        return ( *pr.second / *pr.first ) > k;
+        return ( *pr.second / *pr.first ) > k; ///> maximum / minimum > threshold ?
     };
 
+    /**
+     * MonteCarloSimulation_alpha is the alpha version of the MonteCarloSimulation class.
+     * It now parametrizes the y-axis (2nd template parameter) and has one Distribution.
+     */
     MonteCarloSimulation_alpha<double, double, DIST::UniformReal> monteCarloSimulation(
             10'000'000,
             condition_met,
